38b566c39746cbc321c89e5dfcb67e6ce54bd24b
YamaD <me@dyama.net>
2022-11-22 17:42:20 +0900

    a

---
+++ src/bitmap.d
@@ -0,0 +1,24 @@
+import std;
+
+class BMP {
+}
+
+auto unpack(T, U: ubyte)(U[] buf) {
+  auto fixed = cast(U[T.sizeof])buf[0..T.sizeof];
+  return fixed.littleEndianToNative!T;
+}
+
+auto parse(T: ubyte)(T[] buf) {
+  auto offs   = buf[0x0a..0x0e].unpack!uint;
+  auto width  = buf[0x12..0x16].unpack!int;
+  auto height = buf[0x16..0x1a].unpack!int;
+
+  if(buf[0x1c..0x1e].unpack!ushort != 32)
+    fatal("format is not supported");
+
+  auto data = buf[offs..$]
+    .chunks(4).map!fromBGRA
+    .chunks(width).array.reverse;
+
+  return BMP(data);
+}
---
+++ src/main.d
@@ -0,0 +1,6 @@
+import bitmap;
+import std;
+
+void main(string[] args) {
+  auto hoge = import("frame0.bmp");
+}
